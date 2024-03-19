33556a233dc52da4b217a1bd5e9bd4002a11c7eb
willpch <lolplayerbr@gmail.com>
2024-03-18 22:04:38 -0300

    Fix img

--- resources/views/home/welcome.blade.php
+++ resources/views/home/welcome.blade.php
@@ -182,7 +182,7 @@
                 <div class="col-lg-5 col-12">
                     <div class="artists-thumb">
                         <div class="artists-image-wrap">
-                            <img src="{{ asset('images/artists/sabrina_1.jpg') }}" alt="imagem sabrina" class="artists-image img-fluid">
+                            <img src="{{ asset('images/sabrina_1.jpg') }}" alt="imagem sabrina" class="artists-image img-fluid">
                         </div>
 
                         <div class="artists-hover">
@@ -206,7 +206,7 @@
                     </div>
                     <div class="artists-thumb">
                         <div class="artists-image-wrap">
-                            <img src="{{ asset('images/artists/Jesus_1.jpg') }}" alt="Imagem Jesus"
+                            <img src="{{ asset('images/Jesus_1.jpg') }}" alt="Imagem Jesus"
                                  class="artists-image img-fluid">
                         </div>
 
@@ -240,7 +240,7 @@
 
                 <div class="col-lg-5 col-12">
                     <div class="artists-thumb">
-                        <img src="images/artists/int_3.jpg"
+                        <img src="images/int_3.jpg"
                              class="artists-image img-fluid">
 
                         <div class="artists-hover">
@@ -269,7 +269,7 @@
                     </div>
 
                     <div class="artists-thumb">
-                        <img src="images/artists/int_2.jpg"
+                        <img src="images/int_2.jpg"
                              class="artists-image img-fluid">
 
                         <div class="artists-hover">
